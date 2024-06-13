956105cecaacd5d310e3e7731dfb19a83a34a978
Mirza Abdijanovic <mirzaabdijanovic9@gmail.com>
2024-06-13 15:24:42 +0200

    Initial

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE todo_app;
+
+USE todo_app;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description TEXT NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
